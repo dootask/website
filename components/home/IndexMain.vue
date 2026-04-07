60581f0c6e1a581addd6aae74cf0f0afca87459f
kuaifan <aipaw@live.cn>
2026-04-07 04:43:27 +0000

    feat: add 'Free Trial' button linking to saas.dootask.top

--- components/home/IndexMain.vue
+++ components/home/IndexMain.vue
@@ -45,6 +45,11 @@
               {{ $t('homepage.hero.deployment') }}
             </button>
           </a>
+          <a href="https://saas.dootask.top" target="_blank">
+            <button class="btn btn-primary">
+              {{ $t('common.free_trial') }}
+            </button>
+          </a>
         </div>
       </div>
       <div class="ten_img">
